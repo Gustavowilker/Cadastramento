297c628b93ea1a3599ee8e7668bc6876bab2919a
Gustavo Wilker <gustavowilkeravelar@gmail.com>
2022-04-05 15:06:07 -0300

    criando uma loginPage

---
+++ lib/Pages/loginPage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class loginPage extends StatefulWidget {
+  const loginPage({Key? key}) : super(key: key);
+
+  @override
+  State<loginPage> createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+  String email = '';
+  String password = '';
+
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset('assests/imagens/LOGO-HORIZONTAL-01.png'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.2),
+          ),
+        ],
+      ),
+    );
+  }
+}
