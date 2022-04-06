d962ed5d35a9d5d49a6d9f1fd42349a696664a48
Gustavo Wilker <gustavowilkeravelar@gmail.com>
2022-04-06 15:58:33 -0300

    Update cadastre.dart

--- lib/Pages/loginPage/cadastre.dart
+++ lib/Pages/loginPage/cadastre.dart
@@ -1,21 +1,59 @@
 import 'package:flutter/material.dart';
 
 class cadastre extends StatefulWidget {
-  const cadastre({ Key? key }) : super(key: key);
+  const cadastre({Key? key}) : super(key: key);
 
   @override
   State<cadastre> createState() => _cadastreState();
 }
 
 class _cadastreState extends State<cadastre> {
+  String nome = '';
+  String email = '';
+  String password = '';
+  String funcao = '';
+  int cpf = 0;
+  int matricula = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cadastre-se'),
       ),
-      body: Card(
-        
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              child: Text(
+                'Informe seus dados e Matricula para ter acesso às Tarefas:',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Card(
+            color: Color.fromARGB(255, 174, 200, 212),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                children: [
+                  Text(
+                    'Nome Completo:',
+                    textAlign: TextAlign.left,
+                  ),
+                  TextField(
+                    onChanged: (Text) {
+                      nome = Text;
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'ex.: Maria dos Santos Santos'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
